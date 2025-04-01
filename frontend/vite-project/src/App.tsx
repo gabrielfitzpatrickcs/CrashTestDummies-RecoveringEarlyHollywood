e596afff0f0fe54488674a09d3717f63b7560f4f
Michael <98921621+Mikee1243@users.noreply.github.com>
2025-04-01 12:05:24 -0400

    Can now select from list below search bar to fill search bar

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -4,13 +4,18 @@ import SearchBar from './components/SearchBar'
 import InformationBelowSearchbar from './components/InformationBelowSearchbar'
 
 function App() {
+  let items = ["search", "item1", "item2", "item3"]
+
+  const handleSelectHistory = (item: string) => {
+    console.log(item);
+  }
 
   return (
     <>
       <div>
         <NavBar />
         <div className="container">
-          <SearchBar />
+          <SearchBar items={items} title='Search Bar' onSelectHistory={handleSelectHistory} />
         </div>
         <InformationBelowSearchbar />
       </div>
--- frontend/vite-project/src/components/SearchBar.tsx
+++ frontend/vite-project/src/components/SearchBar.tsx
@@ -1,11 +1,50 @@
-function SearchBar() {
+import React, { useState } from "react";
+import { MouseEvent } from "react";
+import { ChangeEvent } from "react";
+
+interface SearchBarProps {
+    items: string[];
+    title: string;
+    onSelectHistory: (item: string) => void;
+}
+
+
+function SearchBar({ items, title, onSelectHistory }: SearchBarProps) {
+    const [input, setInput] = useState("")
+    let currentSelected = "";
+
+    //Event Handler
+    // const handleClick = (e: MouseEvent) => { console.log(item) 
+    const handleClick = (item: string) => {
+        console.log(item)
+        currentSelected = item
+        setInput(currentSelected)
+    }
+    //items = [];
+
+    // const onChange = (e: ChangeEvent, value: string) => {
+    //     setInput(e.target.value)
+    // }
+
     return (
         <>
-            <div className="m-5">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Searchbar Title</label>
-                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Search the archive" />
-            </div>
-
+            <div className="input-wrapper m-5">
+                <label htmlFor="exampleFormControlInput1" className="form-label">{title}</label>
+                <input className="form-control" placeholder="Search the archive" value={input} onChange={(e) => setInput(e.target.value)} />
+                <div className="history-list">
+                    <ul className="list-group">
+                        {items.map((item, index) => (
+                            <li
+                                className={"list-group-item"}
+                                key={item}
+                                onClick={() => { handleClick(item) }}
+                            >
+                                {item}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div >
         </>
     );
 }
