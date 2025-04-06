45aa3dcb9180aa1fdc5e8a69090c12ce1339acc6
gabrielfitzpatrickcs <gabrielfitzpatrick.cs@gmail.com>
2025-04-06 15:53:38 -0400

    Finalize Firebase config with proper imports and exports

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+import { getAuth } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // import { getAuth, updateProfile, updateEmail, updatePassword, deleteUser, setPersistence, browserSessionPersistence } from 'firebase/auth';
-import { doc, getDoc, getFirestore, updateDoc, deleteDoc, collection } from 'firebase/firestore';
+import { doc, getDoc, updateDoc, deleteDoc, collection } from 'firebase/firestore';
 // import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
